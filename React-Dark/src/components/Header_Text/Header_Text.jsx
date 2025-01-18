9df3e64495d67af5ab2f93d290a0e9109d921751
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-18 22:37:27 +0330

    update header text

---
+++ React-Dark/src/components/Header_Text/Header_Text.jsx
@@ -0,0 +1,15 @@
+import "./Header_Text.css"
+
+const HeeaderText = () => {
+  return (
+    <div className="HeaderText_Wrapper">
+      <h2> Unloke the Power of Dark: </h2>
+
+      <button className="Button1_Header"> Discover the Difference </button>
+      <br />
+      <button className="Button2_Header"> Experience the Extraordinary </button>
+    </div>
+  )
+}
+
+export default HeeaderText
--- React-Dark/src/page/FirstPage/FirstPage.jsx
+++ React-Dark/src/page/FirstPage/FirstPage.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header"
+import HeeaderText from "../../components/Header_Text/Header_Text"
 import "./FirstPage.css"
 
 const FirstPage = () => {
   return (
     <div className="First_Page">
       <Header className="Header" />
+      <HeeaderText />
     </div>
 )
 }
