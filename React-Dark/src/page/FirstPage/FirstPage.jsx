c50388de72ce05b9ae13b2dee07bf0e3a679b2b3
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-18 22:13:38 +0330

    update header css

--- React-Dark/src/page/FirstPage/FirstPage.jsx
+++ React-Dark/src/page/FirstPage/FirstPage.jsx
@@ -1,9 +1,10 @@
 import Header from "../../components/Header/Header"
+import "./FirstPage.css"
 
 const FirstPage = () => {
   return (
     <div className="First_Page">
-      <Header />
+      <Header className="Header" />
     </div>
 )
 }
