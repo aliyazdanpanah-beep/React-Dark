dc2db1993e2ed9923e82214abdd92a089bfb377d
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-19 15:26:26 +0330

    complat secend page

--- React-Dark/src/components/Mine/MineRight.jsx
+++ React-Dark/src/components/Mine/MineRight.jsx
@@ -3,7 +3,14 @@ import "./MineRight.css"
 const MineRight = () => {
   return (
     <div className="MineRight_Wrapper">
-     this is mine Right
+      <button className="MineRight_BTN">Uncover the Extraordinary</button>
+
+      <h1>Delve into the Darkness</h1>
+
+      <p>Unlock the Mysteries of the Dark: Explore our Captivating Homepage</p>
+
+      <p>Unveil the Secrets of the Dark: Embark on a Journey of Exploration through Our Captivating Homepage</p>
+      
     </div>
   )
 }
