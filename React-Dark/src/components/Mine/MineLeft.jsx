169a60329a1d3134cc5811c4450ebdc3fc333e81
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-19 15:04:11 +0330

    complate mine left

--- React-Dark/src/components/Mine/MineLeft.jsx
+++ React-Dark/src/components/Mine/MineLeft.jsx
@@ -1,9 +1,16 @@
+import photo from "../../assets/Image/Liberey.jpg"
 import "./MainLeft.css"
 
 const MineLeft = () => {
   return (
      <div className="MineLeft_Wrapper">
-       This is Mine Left
+       <h1>Immerse Yourself in the Allure of Darkness: Unveil the Secrets of Our Captivating Homepage</h1>
+
+       <img className="image" src= {photo} />
+
+       <h1 className="MineLeft_h1">Embrace the Allure</h1>
+
+       <p>Unveil the Beauty of the Dark: Discover the Captivating Secrets of Our Homepage</p>
      </div>
   )
 }
