f9f842c230986c6917c2e7d3efc6d300e9f0beba
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-19 13:33:47 +0330

    Update Header.jsx

--- React-Dark/src/components/Header/Header.jsx
+++ React-Dark/src/components/Header/Header.jsx
@@ -4,32 +4,8 @@ const Header = () => {
 
   // const Icon = document.getElementById('icon')
   // const Navbar = document.getElementById('Navbar')
-
-  const Icon = () => {
-    const Navbar = document.getElementById('Navbar')
-
-    if (Navbar.style.top == '-200px') {
-
-      Navbar.style.top = '0'
-      Navbar.style.transition = 'all ease-in 1s'
-    }
-    else {
-
-      Navbar.style.top = '-200px'
-      Navbar.style.transition = 'all ease-in 1s'
-    }
-  }
-
   return (
     <div className="header_wrapper">
-
-      <div id="Navbar">
-        <span>Home</span>
-        <span>New</span>
-        <span>Job</span>
-      </div>
-
-     
       <div className="header_part"> <span>DarkHome</span> </div>
 
       <div className="header_part">
