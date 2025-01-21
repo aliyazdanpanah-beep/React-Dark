ce18ee69fef6097aa378321d26736dcde8d4da9c
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-21 20:08:03 +0330

    update secend page

--- React-Dark/src/page/SecendPage/SecndPage.jsx
+++ React-Dark/src/page/SecendPage/SecndPage.jsx
@@ -1,5 +1,6 @@
 import MineLeft from "../../components/Mine/MineLeft"
 import MineRight from "../../components/Mine/MineRight"
+import { useEffect, useState } from "react"
 import "./SeendPage.css"
 
 const SecendPage = () => {
