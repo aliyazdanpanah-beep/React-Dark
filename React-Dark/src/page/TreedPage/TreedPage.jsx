3b1f336cad88dd55c0d02309e28b958e93f41d9e
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-23 13:50:54 +0330

    complat Last page

--- React-Dark/src/page/TreedPage/TreedPage.jsx
+++ React-Dark/src/page/TreedPage/TreedPage.jsx
@@ -4,11 +4,22 @@ import "./TreedPage.css";
 import Text from "../../components/TreedPage/TreedPageText";
 
 const TreedPage = () => {
+  const [api, setApi] = useState([]);
+
+  useEffect(() => {
+    setApi([
+      {
+        id: 1,
+        titel: "ALI",
+      },
+    ]);
+  }, []);
+
   return (
     <div>
       <Text />
-    </div>     
-  )
+    </div>
+  );
 };
 
 export default TreedPage;
