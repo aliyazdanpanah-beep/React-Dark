9ca461b01c6d5801f95deadeb70fd1d054335c59
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-26 22:00:17 +0330

    Add Link in routs

--- React-Dark/src/components/Header/Header.jsx
+++ React-Dark/src/components/Header/Header.jsx
@@ -1,23 +1,23 @@
-import "./Header.css"
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-
   // const Icon = document.getElementById('icon')
   // const Navbar = document.getElementById('Navbar')
   return (
     <div className="header_wrapper">
-      <div className="header_part"> <span>DarkHome</span> </div>
+      <div className="header_part">
+        <Link className=" Links" to={"/"}>Home</Link>
+      </div>
 
       <div className="header_part">
-        <span>Home</span>
-        <span>About</span>
+        <Link className=" Links" to={"/about"}>About</Link>
         <span>Product</span>
         <span>Cotact</span>
         <span>FAQ</span>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- React-Dark/src/page/About/About.jsx
+++ React-Dark/src/page/About/About.jsx
@@ -1,6 +1,10 @@
+import Header from "../../components/Header/Header"
+
+
 const About = () => {
   return (
     <div className="About_Page">
+      <Header />
       <h2>this is About</h2>
     </div>
   )
