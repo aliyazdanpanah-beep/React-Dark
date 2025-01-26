5ff2d874f2c3307cec3e8d55ef7f192079c471fe
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-26 21:30:44 +0330

    create Home

--- React-Dark/src/App.jsx
+++ React-Dark/src/App.jsx
@@ -1,6 +1,7 @@
 import FirstPage from "./page/FirstPage/FirstPage";
 import SecendPage from "./page/SecendPage/SecndPage";
 import TreedPage from "./page/TreedPage/TreedPage";
+import { BrowserRouter } from "react-router-dom"
 
 const App = () => {
   return (
---
+++ React-Dark/src/page/Home/Home.jsx
@@ -0,0 +1,15 @@
+import FirstPage from "../FirstPage/FirstPage"
+import SecendPage from "../SecendPage/SecndPage"
+import TreedPage from "../TreedPage/TreedPage"
+
+const Home = () => {
+return (
+  <div className="Home_Section">
+     <FirstPage />
+     <SecendPage />
+     <TreedPage />
+  </div>
+)
+}
+
+export default Home
