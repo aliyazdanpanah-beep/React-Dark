ca1af28258c9771d9f0389d76b0e77a704f054e3
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-01-26 22:13:03 +0330

    update fourPage

--- React-Dark/src/page/FourdPage/FourdPage.jsx
+++ React-Dark/src/page/FourdPage/FourdPage.jsx
@@ -1,7 +1,30 @@
+import "./FourdPage.css"
+import { Link } from "react-router-dom"
+
 const FourdPage = () => {
   return (
     <div className="FourPage">
-      
+      <div className="Links_wrapper">
+        <span>Link</span>
+        <br />
+        <span>Link</span>
+        <br />
+        <span>Link</span>
+        <br />
+        <span>Link</span>
+      </div>
+      <div className="Links_wrapper">
+        <span>Link</span>
+        <span>Link</span>
+        <span>Link</span>
+        <span>Link</span>
+      </div>
+      <div className="Links_wrapper">
+        <span>Link</span>
+        <span>Link</span>
+        <span>Link</span>
+        <span>Link</span>          
+      </div>
     </div>
   )
 }
